44738d044813a25ee85ecaed3f819f0a044a2d1f
ShamnaKunnath <shamnakunnath54@gmail.com>
2024-05-03 20:43:46 +0530

    Assignment1: Percolation

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -6,19 +5,21 @@ public class Percolation {
     private int size;
     private WeightedQuickUnionUF weightedQuickUnionUF;
     private int countOfOpenSites;
+    private WeightedQuickUnionUF quickUnionUF;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
-           throw  new IllegalArgumentException("n should be greater than 0");
+            throw  new IllegalArgumentException("n should be greater than 0");
         }
         size = n;
-        grid = new boolean[n * n + 1];
+        grid = new boolean[n * n + 2];
         grid[0] = true;
-        for (int i = 1; i < n * n + 1; i++) {
+        for (int i = 1; i < n * n + 2; i++) {
             grid[i] = false;
         }
-        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 1);
+        quickUnionUF = new WeightedQuickUnionUF(n * n + 2);
     }
 
     // opens the site (row, col) if it is not open already
@@ -29,32 +30,41 @@ public class Percolation {
         if (col < 1 || col > size) {
             throw new IllegalArgumentException("column indices should be between 1 and n");
         }
-        countOfOpenSites++;
-        grid[serializedIndex(row, col)] = true;
+        if (!isOpen(row, col)) {
+            countOfOpenSites++;
+            grid[this.serializedIndex(row, col)] = true;
 
-        if (row < size && grid[serializedIndex(row + 1, col)]) {
-            weightedQuickUnionUF.union(serializedIndex(row, col), serializedIndex(row +  1, col));
-        }
-        if (row > 1 && grid[serializedIndex(row - 1, col)]) {
-            weightedQuickUnionUF.union(serializedIndex(row, col), serializedIndex(row -  1, col));
-        }
-        if (col < size && grid[serializedIndex(row, col + 1)]) {
-            weightedQuickUnionUF.union(serializedIndex(row, col), serializedIndex(row, col + 1));
-        }
-        if (col > 1 && grid[serializedIndex(row, col - 1)]) {
-            weightedQuickUnionUF.union(serializedIndex(row, col), serializedIndex(row, col - 1));
-        }
+            if (row < size && this.isOpen(row + 1, col)) {
+                weightedQuickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row +  1, col));
+                quickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row +  1, col));
 
-        //connects the top to arbitrary object at 0
-        if (row == 1) {
-            weightedQuickUnionUF.union(0, serializedIndex(row, col));
-        }
+            }
+            if (row > 1 && this.isOpen(row - 1, col)) {
+                weightedQuickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row -  1, col));
+                quickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row -  1, col));
+            }
+            if (col < size && this.isOpen(row, col + 1)) {
+                weightedQuickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row, col + 1));
+                quickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row, col + 1));
+            }
+            if (col > 1 && this.isOpen(row, col - 1)) {
+                weightedQuickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row, col - 1));
+                quickUnionUF.union(this.serializedIndex(row, col), this.serializedIndex(row, col - 1));
+            }
 
-        //connects the bottom to arbitrary object at n*n+1
-        if (row == size && this.isFull(row, col)) {
-            weightedQuickUnionUF.union(size * size + 1, serializedIndex(row, col));
-        }
+            // connects the top to arbitrary object at 0
+            if (row == 1) {
+                weightedQuickUnionUF.union(this.serializedIndex(row, col), 0);
+                quickUnionUF.union(this.serializedIndex(row, col), 0);
+            }
 
+            // connects the bottom to arbitrary object at n*n+1
+            if (row == size) {
+                // weightedQuickUnionUF.union(size * size + 1, this.serializedIndex(row, col));
+                quickUnionUF.union(this.serializedIndex(row, col), size * size + 1);
+
+            }
+        }
     }
 
     private int serializedIndex(int i, int j) {
@@ -68,7 +78,7 @@ public class Percolation {
         if (col < 1 || col > size) {
             throw new IllegalArgumentException("column indices should be between 1 and n");
         }
-        return grid[serializedIndex(row, col)];
+        return grid[this.serializedIndex(row, col)];
     }
 
     // is the site (row, col) full?
@@ -79,42 +89,24 @@ public class Percolation {
         if (col < 1 || col > size) {
             throw new IllegalArgumentException("column indices should be between 1 and n");
         }
-        //return weightedQuickUnionUF.connected(serializedIndex(row,col),0);
-        return weightedQuickUnionUF.find(serializedIndex(row, col)) == 0;
+        return weightedQuickUnionUF.connected(this.serializedIndex(row, col), 0);
+        // return weightedQuickUnionUF.find(this.serializedIndex(row, col)) == 0;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return countOfOpenSites;
+        return this.countOfOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return weightedQuickUnionUF.find(size * size + 1) == 0;
-        //return weightedQuickUnionUF.connected(0,size*size+1);
+        return quickUnionUF.connected(0, size * size + 1);
+        // return quickUnionUF.find(size * size + 1) == 0;
+        // return weightedQuickUnionUF.connected(0,size*size+1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation percolation = new Percolation(3);
-        StdOut.println(percolation.percolates());
-        StdOut.println(percolation.isFull(1, 1));
-        //StdOut.println(percolation.numberOfOpenSites());
-        percolation.open(1, 1);
-        //StdOut.println(percolation.percolates());
-        //StdOut.println(percolation.numberOfOpenSites());
-        StdOut.println(percolation.isFull(1, 1));
-        percolation.open(2, 1);
-        //StdOut.println(percolation.percolates());
-        //StdOut.println(percolation.numberOfOpenSites());
-        StdOut.println(percolation.isFull(2, 1));
-        StdOut.println(percolation.isFull(3, 1));
-        percolation.open(2, 2);
-        StdOut.println(percolation.isFull(1,3));
-        percolation.open(3, 3);
-        StdOut.println(percolation.isFull(3, 3));
-        StdOut.println(percolation.percolates());
-        //StdOut.println(percolation.numberOfOpenSites());
-        //StdOut.println(percolation.percolates());
+
     }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,66 +1,67 @@
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private Percolation system;
+    //private Percolation system;
     private double[] plot;
     private int t;
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials){
-        if (n<=0 || trials<=0){
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException("Constructors should be positive");
         }
         plot = new double[trials];
-        t= trials;
-        for (int i =0;i<trials;i++){
-            system = new Percolation(n);
-            while(!system.percolates()){
-                int x=StdRandom.uniformInt(1,n+1);
-                int y=StdRandom.uniformInt(1,n+1);
-                if (!system.isOpen(x,y)){
-                    system.open(x,y);
+        t = trials;
+        for (int i = 0; i < trials; i++) {
+            Percolation system = new Percolation(n);
+            while (!system.percolates()) {
+                int x = StdRandom.uniformInt(1, n + 1);
+                int y = StdRandom.uniformInt(1, n + 1);
+                if (!system.isOpen(x, y)) {
+                    system.open(x, y);
                 }
             }
-            plot[i]=(double) system.numberOfOpenSites()/(n*n);
+            plot[i] = (double) system.numberOfOpenSites() / (n * n);
 
         }
 
     }
 
     // sample mean of percolation threshold
-    public double mean(){
+    public double mean() {
         return StdStats.mean(plot);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(plot);
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo(){
-        return this.mean()-(1.96*this.stddev()/Math.sqrt(t));
+    public double confidenceLo() {
+        return this.mean() - (1.96 * this.stddev() / Math.sqrt(t));
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
-        return this.mean()+(1.96*this.stddev()/Math.sqrt(t));
+    public double confidenceHi() {
+        return this.mean() + (1.96 * this.stddev() / Math.sqrt(t));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        //StdOut.println("Enter n");
+        // StdOut.println("Enter n");
 
-        int N= StdIn.readInt();
-        //StdOut.println("Enter no of trials");
-        int T= StdIn.readInt();
+        // int N = StdIn.readInt();
+        int N = Integer.parseInt(args[0]);
+        // StdOut.println("Enter no of trials");
+        // int T = StdIn.readInt();
+        int T= Integer.parseInt(args[1]);
 
-        PercolationStats percolationStats = new PercolationStats(N,T);
-        StdOut.println("mean\t"+"= "+percolationStats.mean()+"\n"+
-                "stddev\t"+"= "+percolationStats.stddev()+"\n"+
-                "95% confidence interval\t"+"= ["+percolationStats.confidenceLo()+","+percolationStats.confidenceHi()+"]"+"\n"
+        PercolationStats percolationStats = new PercolationStats(N, T);
+        StdOut.println("mean\t" + "= " + percolationStats.mean() + "\n"
+                + "stddev\t" + "= " + percolationStats.stddev() + "\n"
+                + "95% confidence interval\t" + "= [" + percolationStats.confidenceLo() + "," + percolationStats.confidenceHi() + "]" + "\n"
         );
     }
 }
